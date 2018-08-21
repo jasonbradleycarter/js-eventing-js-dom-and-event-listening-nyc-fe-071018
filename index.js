0a907f1795b0bc08ae7950e988edaf2b62aecfdd
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-21 23:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const main = document.querySelector('#main');
+const paragraph = document.querySelector('p');
 
 main.addEventListener('click', (event) => {
   alert('You clicked me!');
+});
+
+paragraph.addEventListener('click', (event) => {
+  alert('You clicked the p!!!');
+  paragraph.style.backgroundColor = 'yellow';
 });
 
